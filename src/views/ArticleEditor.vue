b0569613a0e8642a83a469f1b5f78396616a9be3
lcyjtz <lcyjtz@qq.com>
2020-08-29 16:07:28 +0800

    "引用基本js css"

--- src/views/ArticleEditor.vue
+++ src/views/ArticleEditor.vue
@@ -1,6 +1,11 @@
 <template>
     <div>11111</div>
 </template>
-<style scoped>
+<style scoped lang="less">
 
 </style>
+<script>
+export default {
+    
+}
+</script>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,30 @@
 <template>
     <div>11112</div>
 </template>
-<style scoped>
-
+<style scoped lang="less">
+body,html{
+     width: 100%;
+     height: auto;
+}
+@import "../assets/css/articleeditor.css";
+@import "../assets/css/backStyle.css";
+@import "../assets/css/boxsstyle.css";
+@import "../assets/css/button.css";
+@import "../assets/css/flowlayout.css";
+@import "../assets/css/styles.css";
+@import "../assets/css/tab.css";
+@import "../assets/css/textbackgrame.css";
 </style>
+<script>
+import "../assets/js/article.js"
+import "../assets/js/item.js"
+import "../assets/js/jquery.ckLine.js"
+import { onMounted } from 'vue'
+export default {
+    setup(){
+        onMounted(()=>{
+            
+        })
+    }
+}
+</script>
